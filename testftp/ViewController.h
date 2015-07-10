845902bdf0768629156b235b56bfe68dcf62e00d
AndLi <ww1095@163.com>
2015-07-10 17:12:04 +0800

    Initial Commit

---
+++ testftp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testftp
+//
+//  Created by AndLi on 15/7/10.
+//  Copyright (c) 2015年 AndLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testftp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testftp
+//
+//  Created by AndLi on 15/7/10.
+//  Copyright (c) 2015年 AndLi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
